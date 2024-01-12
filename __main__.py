cc5e8399382f5c5f7963bc770dcb8fc02ae17a43
siracencoserghei <siracencoserghei@gmail.com>
2024-01-12 21:25:15 +0100

    init from hw2

---
+++ __main__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from actions import run_file_manager
+
+if __name__ == "__main__":
+    run_file_manager()
+    
+
---
+++ actions.py
@@ -0,0 +1,5 @@
+
+def run_file_manager():
+    from FileManagerBot import FileManagerBot
+    bot = FileManagerBot()
+    bot.run()
